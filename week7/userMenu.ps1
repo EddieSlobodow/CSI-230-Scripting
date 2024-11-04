6742ff631214329bb1dba459c5bdb6ef48e0aeb4
Eddie <edmond.slobodow@mymail.champlain.edu>
2024-11-03 23:16:36 -0500

    turn to menu

--- week7/userMenu.ps1
+++ week7/userMenu.ps1
@@ -18,13 +18,29 @@ while (-not $exit) {
     switch ($selection) {
         1 { ApacheLogs1 | Select-Object -First 10 | Format-Table -AutoSize -Wrap }
 
-        2 { getFailedLogins }
+        2 { getFailedLogins(999) | Select-Object -First 10 | Format-Table -AutoSize -Wrap }
 
-        3 { }
+        3 { 
+            $days = Read-Host -Prompt "Enter the number of days to check the logs for"
+            $userLogins = getFailedLogins $days
+            $failedUser = @()
+            $hashMap = @{}
+            for ($i = 0; $i -lt $userLogins.count; $i++){
+                if ($hashMap.ContainsKey($userLogins[$i].User)) {$hashMap[$userLogins[$i].User] += 1}
+                else {$hashMap[$userLogins[$i].User] = 1}
+            }
+            foreach ($key in $hashMap.Keys) {
+                if ($hashMap[$key] -ge 10) {failedUser += $key}
+            }
 
-        4 { }
+             Write-Host $failedUser
+            }
+
+        4 { Start-Process -FilePath "chrome" '-ArgumentList --start-fullscreen "http://www.champlain.edu"'}
 
         5 { $exit = $true }
+
+        Default { Write-Host "Enter a number 1-5" }
     }
 }
 
